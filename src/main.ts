253000dae341f1b8efe6b3e060438f722c06759a
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2024-05-22 16:33:36 -0300

    Update CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-//import * as cors from 'cors';
+import * as cors from 'cors';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors({
-    origin: 'https://cadastro-front-end-com-backend-nestjs.netlify.app', // Domínio da origem permitida
-    credentials: true, // Permita o compartilhamento de credenciais
-  });
-  app.useGlobalPipes(new ValidationPipe(
-    {
-      whitelist:true
-    }
-  ))
-  await app.listen(process.env.PORT ||3333);
+
+  app.use(cors({
+    origin: 'https://cadastro-front-end-com-backend-nestjs.netlify.app',
+    credentials: true,
+  }));
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+  }));
+
+  await app.listen(process.env.PORT || 3333);
 }
+
 bootstrap();
