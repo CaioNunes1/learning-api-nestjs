6f0ca3368111ce87c03f090ad7b141c336ef9ec2
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2024-05-23 10:47:10 -0300

    changes on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe, ForbiddenException, Logger } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const whitelist = [
+    'https://cadastro-front-end-com-backend-nestjs.netlify.app',
+    'http://localhost:5173',
+  ];
+
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://cadastro-front-end-com-backend-nestjs.netlify.app', // Permita a origem do Netlify
-    credentials: true, // Permitir credenciais
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    origin: function (origin, callback) {
+      if (!origin) {
+        callback(null, true);
+        return;
+      }
+      if (whitelist.includes(origin)) {
+        callback(null, true);
+      } else {
+        Logger.warn(`Origin: ${origin} not allowed by CORS`);
+        callback(new ForbiddenException('Not allowed by CORS'), false);
+      }
+    },
+    credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-  }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(cookieParser());
 
-  await app.listen(process.env.PORT || 3333); // Use a porta do ambiente ou 3333 como padrão
+  const port = process.env.PORT || 3333;
+  await app.listen(port);
+  Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
