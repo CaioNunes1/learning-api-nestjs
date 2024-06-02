0d12e10a97d7028007c53165f3a6d6fbeff4556b
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2024-06-02 10:13:40 -0300

    algumas mudanças

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,40 @@
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const whitelist = [
-    'https://cadastro-front-end-com-backend-nestjs.netlify.app/',
-    //'http://localhost:5174',
+    'https://cadastro-front-end-com-backend-nestjs.netlify.app',
+    'http://localhost:5173',
+    'http://localhost:5174',
   ];
 
   app.enableCors({
-    origin: function (origin, callback) {
+    origin: (origin, callback) => {
+      console.log(`Origin: ${origin}`); // Log para verificar a origem
       if (!origin || whitelist.includes(origin)) {
         callback(null, true);
       } else {
+        console.error(`Origin: ${origin} not allowed by CORS`);
         callback(new Error('Not allowed by CORS'));
       }
     },
     credentials: true,
   });
 
+  app.use(cookieParser());
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
 
-  await app.listen(process.env.PORT || 3333);
+  const port = process.env.PORT || 3333;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
