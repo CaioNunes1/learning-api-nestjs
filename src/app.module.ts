1b647cb2ee5ecd870aedfd296e2bdd620e8bc190
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2024-06-03 10:44:43 -0300

    teste para deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
-
 @Module({
   imports: [
     AuthModule, 
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  const whitelist = [
-    'https://cadastro-front-end-com-backend-nestjs.netlify.app',
-    'http://localhost:5173',
-    'http://localhost:5174',
-  ];
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: (origin, callback) => {
-      console.log(`Origin: ${origin}`); // Log para verificar a origem
-      if (!origin || whitelist.includes(origin)) {
-        callback(null, true);
-      } else {
-        console.error(`Origin: ${origin} not allowed by CORS`);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: ['https://cadastro-front-end-com-backend-nestjs.netlify.app', 'http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-
-  app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
 
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  await app.listen(process.env.PORT || 3333);
 }
-
 bootstrap();
